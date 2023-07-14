881fd7bf2fc624b02874a3ae2f9a87bd73fffc29
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-07-13 18:22:39 -0700

    - kel

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var image = document.getElementById("flower");
-function createPicture(){
-    image.src = "https://images.unsplash.com/photo-1530092285049-1c42085fd395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwZmxvd2VyfGVufDB8fDB8fHww&w=1000&q=80";
+var dataList;
+var image = document.getElementById("agentPic");
+
+function makeButtons(){
+    let names = document.getElementById("names");
+    for(let i = 0; i < dataList.length; i++){
+        let currName = (dataList[i]["displayName"]);
+        let text = document.createTextNode(currName);
+        let button = document.createElement('BUTTON');
+        button.onclick = function(){
+            createPicture(i);
+        }
+        button.appendChild(text); //adds content to button
+        names.appendChild(button); //appends button to div
+
+
+    }
 }
+
+$.get("https://valorant-api.com/v1/agents", function(data, status){
+    console.log(data['data'])
+    dataList = data["data"];
+    makeButtons();
+});
+
+function createPicture(i){
+    image.src = dataList[i]["fullPortraitV2"];
+    // image.style.width = "700px";
+    // image.style.height = "200px";
+}
+
