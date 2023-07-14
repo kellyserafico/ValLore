d2c821ae681bcbf13ffff64d2c34727080e97b55
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-07-13 17:39:16 -0700

    ya

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+var image = document.getElementById("flower");
 function createPicture(){
-    var image = document.getElementById("flower");
-    image.style.display = "block";
+    image.src = "https://images.unsplash.com/photo-1530092285049-1c42085fd395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwZmxvd2VyfGVufDB8fDB8fHww&w=1000&q=80";
 }
